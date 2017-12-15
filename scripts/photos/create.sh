550aa3aba879d92bcee419ab123647427b8858c2
dianamcnulty <dianamcnulty@gmail.com>
2017-12-15 15:37:52 -0500

    photo create working and tested

--- scripts/photos/create.sh
+++ scripts/photos/create.sh
@@ -10,7 +10,8 @@ curl "${API}${URL_PATH}" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "photo": {
-      "text": "'"${TEXT}"'"
+      "url": "'"${URL}"'",
+      "category": "'"${CATEGORY}"'"
     }
   }'
 
